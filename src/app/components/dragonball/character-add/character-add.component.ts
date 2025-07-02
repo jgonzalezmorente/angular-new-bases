b48487119df02eda3ba0518b20f42a8ac1db1c93
jgonzalezmorente <jgonzalezmorente@gmail.com>
2025-07-02 06:40:40 +0200

    primer commit

---
+++ src/app/components/dragonball/character-add/character-add.component.ts
@@ -0,0 +1,32 @@
+import { Component, output, signal } from '@angular/core';
+import type { Character } from '../../../interfaces/charater.interface';
+
+@Component({
+  selector: 'dragonball-character-add',
+  templateUrl: './character-add.component.html',
+})
+export class CharacterAddComponent {
+  name = signal('');
+  power = signal(0);
+
+  newCharacter = output<Character>();
+
+  public addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: Math.floor(Math.random() * 1000),
+      name: this.name(),
+      power: this.power(),
+    };
+    this.newCharacter.emit(newCharacter);
+    this.resetFields();
+  }
+
+  public resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
+}
